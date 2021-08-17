5a494fc3abd700ff369f57695f5175e5b12d81ce
Jay Jhangiani <jaydjhangiani@gmail.com>
2021-08-17 17:19:13 +0530

    assest added

---
+++ assets/data/client.js
@@ -0,0 +1,13 @@
+import Client1 from '../img/drmoons.jpg'
+import Client2 from '../img/drsheeba.jpg'
+import Client3 from '../img/Cnc.jpg'
+import Client4 from '../img/aditidhawan.jpg'
+import Client5 from '../img/BBP.jpg'
+
+export const clientData = [
+    {img: Client1},
+    {img: Client2},
+    {img: Client3},
+    {img: Client4},
+    {img: Client5},
+]
---
+++ assets/data/service.js
@@ -0,0 +1,39 @@
+import Service1 from "../img/web.png" 
+import Service2 from "../img/marketing.png" 
+import Service3 from "../img/seo.png" 
+import Service4 from "../img/domain.png" 
+import Service5 from "../img/email.png" 
+import Service6 from "../img/security.png" 
+
+export const serviceData = [
+    {
+        img:Service1,
+        service:"Web Development",
+        desc:"Get the online presence you deserve"
+    },
+    {
+       img:Service2,
+       service:"Digital Marketing",
+       desc:"Build your online presence with facts"
+    },
+    {
+       img:Service3,
+       service:"Search Engine Optimisation",
+       desc:"Give you website the boost"
+    },
+    {
+       img:Service4,
+       service:"Domain Registration",
+       desc:"Guaranteed Best Price on Domains"
+    },
+    {
+       img:Service5,
+       service:"Email Hosting",
+       desc:"End to End Assitance"
+    },
+    {
+       img:Service6,
+       service:"Web Security",
+       desc:"Sitelock, Codeguard & SSL Certificate"
+    }
+]
